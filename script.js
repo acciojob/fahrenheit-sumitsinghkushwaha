914ba59ae0bc1a103565af0d8a6d87204356520a
Sumit Singh Kushwaha <68112081+sumitsinghkushwaha@users.noreply.github.com>
2023-12-05 17:50:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function toFahrenheit(celsius) {
- // Write your code here
+  // Check if the input temperature is within the valid range
+  if (celsius < -273.15 || celsius > 1.8e9) {
+    // Return an error message or handle it according to your requirements
+    return "Invalid temperature value";
+  }
+
+  // Convert Celsius to Fahrenheit using the formula: F = (C * 9/5) + 32
+  const fahrenheit = (celsius * 9/5) + 32;
+
+  // Return the Fahrenheit temperature with two decimal points of precision
+  return fahrenheit.toFixed(2);
 }
 
-// Do not change the code below
+// Example usage:
 const celsius = prompt("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
